a703066ae9e8dc721415f3da5e5717827abaf568
Miguel de Freitas Muniz <miguimuniz@gmail.com>
2024-09-03 11:49:24 -0300

    feat: adiciona e configura alembic

--- doceria-backend/doceria_backend/models.py
+++ doceria-backend/doceria_backend/models.py
@@ -5,7 +5,7 @@ table_registry = registry()
 
 @table_registry.mapped_as_dataclass
 class Usuario:
-    __tablename__ = 'users'
+    __tablename__ = 'usuarios'
 
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     cliente_id: Mapped[int] = mapped_column(unique=True)
